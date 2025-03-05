238e1ae902ea1665fe22d473965f56c9347a3d89
HarshBhoi36 <harshbhoi3@gmail.com>
2025-03-05 21:44:56 +0530

    Updated server.js
    5 march 9:43

--- backend/server.js
+++ backend/server.js
@@ -179,7 +179,8 @@ app.post("/poke", async (req, res) => {
     }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
